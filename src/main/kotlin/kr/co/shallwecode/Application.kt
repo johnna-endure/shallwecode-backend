a0c31535b34dd5027e75895d10ddb1df2aaf850f
choi woo seok <cwooseok60@gmail.com>
2022-10-29 16:41:19 +0900

    first commit

---
+++ src/main/kotlin/kr/co/shallwecode/Application.kt
@@ -0,0 +1,18 @@
+package kr.co.shallwecode
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kr.co.shallwecode.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
