90c3124a1fb6b0141df383a15bb3abd5185e297c
choi woo seok <cwooseok06@gmai.com>
2022-11-08 22:37:03 +0900

    현재 사용하지 않는 인증 코드 주석처리

--- src/main/kotlin/kr/co/shallwecode/plugins/Security.kt
+++ src/main/kotlin/kr/co/shallwecode/plugins/Security.kt
@@ -24,90 +24,90 @@ fun Application.configureSecurity() {
         }
     }
 
-    authentication {
-        basic(name = "myauth1") {
-            realm = "Ktor Server"
-            validate { credentials ->
-                if (credentials.name == credentials.password) {
-                    UserIdPrincipal(credentials.name)
-                } else {
-                    null
-                }
-            }
-        }
+//    authentication {
+//        basic(name = "myauth1") {
+//            realm = "Ktor Server"
+//            validate { credentials ->
+//                if (credentials.name == credentials.password) {
+//                    UserIdPrincipal(credentials.name)
+//                } else {
+//                    null
+//                }
+//            }
+//        }
+//
+//        form(name = "myauth2") {
+//            userParamName = "user"
+//            passwordParamName = "password"
+//            challenge {
+//                /**/
+//            }
+//        }
+//    }
+//    authentication {
+//        jwt {
+//            val jwtAudience = this@configureSecurity.environment.config.property("jwt.audience").getString()
+//            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
+//            verifier(
+//                JWT
+//                    .require(Algorithm.HMAC256("secret"))
+//                    .withAudience(jwtAudience)
+//                    .withIssuer(this@configureSecurity.environment.config.property("jwt.domain").getString())
+//                    .build()
+//            )
+//            validate { credential ->
+//                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+//            }
+//        }
+//    }
+//    authentication {
+//        oauth("auth-oauth-google") {
+//            urlProvider = { "http://localhost:8080/callback" }
+//            providerLookup = {
+//                OAuthServerSettings.OAuth2ServerSettings(
+//                    name = "google",
+//                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+//                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+//                    requestMethod = HttpMethod.Post,
+//                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
+//                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
+//                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
+//                )
+//            }
+//            client = HttpClient(Apache)
+//        }
+//    }
 
-        form(name = "myauth2") {
-            userParamName = "user"
-            passwordParamName = "password"
-            challenge {
-                /**/
-            }
-        }
-    }
-    authentication {
-        jwt {
-            val jwtAudience = this@configureSecurity.environment.config.property("jwt.audience").getString()
-            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
-            verifier(
-                JWT
-                    .require(Algorithm.HMAC256("secret"))
-                    .withAudience(jwtAudience)
-                    .withIssuer(this@configureSecurity.environment.config.property("jwt.domain").getString())
-                    .build()
-            )
-            validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
-            }
-        }
-    }
-    authentication {
-        oauth("auth-oauth-google") {
-            urlProvider = { "http://localhost:8080/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
-                )
-            }
-            client = HttpClient(Apache)
-        }
-    }
-
-    routing {
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
-        }
-        authenticate("myauth1") {
-            get("/protected/route/basic") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
-            }
-        }
-        authenticate("myauth2") {
-            get("/protected/route/form") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
-            }
-        }
-        authenticate("auth-oauth-google") {
-            get("login") {
-                call.respondRedirect("/callback")
-            }
-
-            get("/callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/hello")
-            }
-        }
-    }
+//    routing {
+//        get("/session/increment") {
+//            val session = call.sessions.get<MySession>() ?: MySession()
+//            call.sessions.set(session.copy(count = session.count + 1))
+//            call.respondText("Counter is ${session.count}. Refresh to increment.")
+//        }
+//        authenticate("myauth1") {
+//            get("/protected/route/basic") {
+//                val principal = call.principal<UserIdPrincipal>()!!
+//                call.respondText("Hello ${principal.name}")
+//            }
+//        }
+//        authenticate("myauth2") {
+//            get("/protected/route/form") {
+//                val principal = call.principal<UserIdPrincipal>()!!
+//                call.respondText("Hello ${principal.name}")
+//            }
+//        }
+//        authenticate("auth-oauth-google") {
+//            get("login") {
+//                call.respondRedirect("/callback")
+//            }
+//
+//            get("/callback") {
+//                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
+//                call.sessions.set(UserSession(principal?.accessToken.toString()))
+//                call.respondRedirect("/hello")
+//            }
+//        }
+//    }
 }
 
 class UserSession(accessToken: String)
