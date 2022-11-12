e85d7f4d72f6f57fd99ce336bdac052bacde315c
choi woo seok <cwooseok60@gmail.com>
2022-11-12 15:08:41 +0900

    exposed, h2 의존성 추가

--- src/main/kotlin/kr/co/shallwecode/plugins/Routing.kt
+++ src/main/kotlin/kr/co/shallwecode/plugins/Routing.kt
@@ -12,29 +12,5 @@ fun Application.configureRouting() {
     }
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
     }
-}
-
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-@Location("/type/{name}")
-data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
 }
