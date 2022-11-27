4cfadf05a4a4f6aa94bcd9e45f1533ea88df28f3
choi woo seok <cwooseok06@gmai.com>
2022-11-27 22:56:15 +0900

    cors 추가

--- src/main/kotlin/kr/co/shallwecode/plugins/HTTP.kt
+++ src/main/kotlin/kr/co/shallwecode/plugins/HTTP.kt
@@ -1,20 +1,18 @@
 package kr.co.shallwecode.plugins
 
 import io.ktor.http.*
-import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
     install(CORS) {
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
         allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+//        allowHeader("MyCustomHeader")
     }
-
 }
