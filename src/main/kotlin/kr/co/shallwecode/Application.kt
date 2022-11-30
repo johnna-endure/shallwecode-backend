0c9d1f7c80e781d6ffcc96b14c2daa1539e2e7de
choi woo seok <cwooseok06@gmai.com>
2022-11-30 13:14:32 +0900

    jwt 인증 수정

--- src/main/kotlin/kr/co/shallwecode/Application.kt
+++ src/main/kotlin/kr/co/shallwecode/Application.kt
@@ -1,6 +1,8 @@
 package kr.co.shallwecode
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kr.co.shallwecode.module.user.userModule
@@ -11,17 +13,36 @@ import kr.co.shallwecode.plugins.configureSerialization
 import org.kodein.di.ktor.di
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+//    embeddedServer(Netty, port = 8080) {
+//        di {
+//            import(userModule)
+//        }
+//
+//        configureHTTP()
+//        configureSecurity()
+//        configureSerialization()
+//        configureRouting()
+//    }.start(wait = true)
+    embeddedServer(Netty, environment = applicationEngineEnvironment {
+        config = HoconApplicationConfig(ConfigFactory.load())
 
-        di {
-            import(userModule)
+        module {
+            di {
+                import(userModule)
+            }
+
+            configureHTTP()
+            configureSecurity()
+            configureSerialization()
+            configureRouting()
         }
-        
-        configureHTTP()
-        configureSecurity()
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+
+
+        connector {
+            port = 8080
+            host = "127.0.0.1"
+        }
+    }).start(wait = true)
 }
 
 fun Application.configureRouting() {
--- src/main/kotlin/kr/co/shallwecode/plugins/Security.kt
+++ src/main/kotlin/kr/co/shallwecode/plugins/Security.kt
@@ -2,17 +2,19 @@ package kr.co.shallwecode.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 
 
 fun Application.configureSecurity() {
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
 
     authentication {
         jwt(AuthenticationConfigName.AUTH_JWT.name) {
-            val secret = this@configureSecurity.environment.config.property("jwt.secret").getString()
-            val issuer = this@configureSecurity.environment.config.property("jwt.issuer").getString()
             verifier(
                 JWT
                     .require(Algorithm.HMAC256(secret))
@@ -21,52 +23,14 @@ fun Application.configureSecurity() {
                     .build()
             )
 
-//            validate { credential ->
-//                if (credential.payload.getClaim("userId").asString() != "") {
-//                    JWTPrincipal(credential.payload)
-//                } else {
-//                    null
-//                }
-//            }
-//
-//            challenge { _, _ ->
-//                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
-//            }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
         }
     }
 
-//
-//    authentication {
-//        session<UserSession>(AuthenticationConfigName.SWC_USER_SESSION.name) {
-//            validate { session ->
-//                val userModel = loginService.login(
-//                    LoginRequest(
-//                        loginId = session.loginId,
-//                        password = session.password
-//                    )
-//                )
-//
-//                if (userModel != null) {
-//                    session
-//                } else {
-//                    null
-//                }
-//            }
-//        }
-//    }
-//
-//    install(Sessions) {
-//        cookie<UserSession>(AuthenticationConfigName.SWC_USER_SESSION.name) {
-//            cookie.path = "/"
-//            cookie.maxAgeInSeconds = 60 * 60 * 24
-////            cookie.httpOnly = true
-//            // TODO 쿠키 도메인 설정 필요
-//            cookie.domain = "localhost"
-//        }
-//    }
 }
 
-//data class UserSession(val loginId: String, val password: String) : Principal
 enum class AuthenticationConfigName {
     AUTH_JWT
 }
